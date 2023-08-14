9f45eb9bf992611c26ebf23b673d48dcf81e7a51
leonsh <detectivehlh@qq.com>
2023-08-14 11:32:07 +0800

    deploy: release new version
    Change-Id: Ie8fce8e1ba76a7a17afe390a292e86d08d84baa4

--- new_post.sh
+++ new_post.sh
@@ -3,13 +3,14 @@ project_path=$(pwd)
 cd ./src/posts || exit
 final_path=$(pwd)
 
+# 通过上次的永久链接生成下一篇文章的永久链接
 get_permalink(){
   date=$(date "+%Y-%m-%d")
   curr_prefix="${date: 2: 2}${date: 5: 2}"
 
   last=$(head -n +1 "$1/latest_permalink.txt")
   if [ "${last: 0: 4}" = "$curr_prefix" ]; then
-    echo "${curr_prefix}$((${last: 4: 1}+1))"
+    echo "${curr_prefix}$((${last: 4}+1))"
   else
     echo "${curr_prefix}1"
   fi
@@ -25,6 +26,7 @@ if [ -z "$filename" ]; then
   exit 1
 fi
 
+# 新文章的 formatter 模版
 article_temp='---
 date: '${date}'
 permalink: /posts/'${permanent_number}'.html
@@ -33,7 +35,11 @@ category:
 ---'
 
 new_post_dir=$project_path/src/.vuepress/public/images/${category}/${permanent_number}
+# 为新文章创建单独的文件夹
 mkdir "$new_post_dir"
+# 打开这个目录, 方便往里面丢图片
 open "$new_post_dir"
+# 更新「上次永久链接」的值
 echo "$permanent_number" > "$project_path/latest_permalink.txt"
+# 生成文件, 并将 formatter 模版写入到新文件
 echo "$article_temp" > "$final_path/$category/$filename".md
