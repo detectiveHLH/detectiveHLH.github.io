7fa355428d8ace4d3aac41a3934c45b04f674cae
leonsh <detectivehlh@qq.com>
2023-09-02 21:24:47 +0800

    deploy: release new version

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-rm -rf src/.vuepress/dist
-
-./compress_image.sh
-
-yarn docs:build
-
-cp -r src/.vuepress/dist/* ./
+#rm -rf src/.vuepress/dist
+#
+#./compress_image.sh
+#
+#yarn docs:build
+#
+#cp -r src/.vuepress/dist/* ./
 
 git add  . && git commit -m "deploy: release new version" && git push origin master
