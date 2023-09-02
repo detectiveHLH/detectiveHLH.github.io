6052a0091ffa0e52deea6cd3faafe1f7421745c7
leonsh <detectivehlh@qq.com>
2023-09-02 22:51:02 +0800

    feat: remove unused code

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#rm -rf src/.vuepress/dist
-#
-#./compress_image.sh
-#
-#yarn docs:build
-#
-#cp -r src/.vuepress/dist/* ./
-
-git add  . && git commit -m "deploy: release new version" && git push origin master
--- src/.vuepress/client.ts
+++
@@ -1,8 +0,0 @@
-import { defineClientConfig } from "@vuepress/client";
-import MyComponent from "./components/MyComponent.vue";
-
-export default defineClientConfig({
-    enhance: ({ app, router, siteData }) => {
-        app.component("MyComponent", MyComponent);
-    },
-});
